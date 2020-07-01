da27ae42153e95d39946de6d8bb9bbd48944f490
Rajil1213 <imrb1213@gmail.com>
2020-07-01 20:07:12 +0545

    Added Comments

--- challenge9.py
+++ challenge9.py
@@ -1,5 +1,14 @@
 def pad(text, size):
+    """pads text to make it of size `size`,
+    according to the PKCS#7 padding scheme
 
+    Args:
+        text (bytes): the text to pad
+        size (int): required size of padded string
+
+    Returns:
+        bytes: the padded string with size = `size`
+    """
     length = len(text)
     if length % size == 0:
         return text
@@ -7,7 +16,7 @@ def pad(text, size):
     padding = size - (length % size)
     padValue = hex(padding).lstrip('0x')
     if len(padValue) == 1:
-        padValue = '0' + padValue
+        padValue = '0' + padValue # bytes can't convert single digit hex
     padValue = bytes.fromhex(padValue)
 
     paddedString = text + padValue * padding
