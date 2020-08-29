9f394a20d6d258bad1f53b021b2afd122fc092dd
Rajil1213 <imrb1213@gmail.com>
2020-08-29 17:01:36 +0545

    Added PKCS#7 padding and ECB-CBC detection function

--- challenge11.py
+++ challenge11.py
@@ -15,20 +15,36 @@ def random_key(length):
         key += bytes(length - len(key) % length)
     return key
 
+
 def pad(msg):
     leftPadCnt = Random().randint(5, 10)
     rightPadCnt = Random().randint(5, 10)
     leftPad = random_key(leftPadCnt)
     rightPad = random_key(rightPadCnt)
-    return leftPad + msg + rightPad
+
+    paddedMsg = leftPad + msg + rightPad
+
+    size = 16
+    length = len(paddedMsg)
+    if length % size == 0:
+        return paddedMsg
+    
+    padding = size - (length % size)
+    padValue = hex(padding).lstrip('0x')
+    if len(padValue) == 1:
+        padValue = '0' + padValue # bytes can't convert single digit hex
+    padValue = bytes.fromhex(padValue)
+    paddedMsg += padValue * padding
+
+    return paddedMsg
+    
 
 def encryption_oracle(msg):
     mode = Random().randint(0, 1)
     key = random_key(16)
     print(f"Key: { key }")
     paddedMsg = pad(msg)
-    if len(paddedMsg) % 16 != 0:
-        paddedMsg += bytes(16 - len(paddedMsg) % 16)
+
     if mode:
         print("Chose ECB Mode")
         cipher = AES.new(key, AES.MODE_ECB)
@@ -38,16 +54,27 @@ def encryption_oracle(msg):
         iv = random_key(16)
         cipher = AES.new(key, AES.MODE_CBC, iv)
         ciphertext = cipher.encrypt(paddedMsg)
+
     return ciphertext
 
+
 def detect(cipher):
-    # to be completed
-    return 
+    chunkSize = 16
+    chunks = []
+    for i in range(0, len(cipher), chunkSize):
+        chunks.append(cipher[i:i+chunkSize])
+
+    uniqueChunks = set(chunks)
+    if len(chunks) > len(uniqueChunks):
+        return "ECB"
+
+    return "CBC"
+
 
 def main():
-    msg = b"Yellow SubmarineTwo One Nine Two"
+    msg = b"Yellow SubmarineTwo One Nine TwoYellow Submarine" * 2
     ciphertext = encryption_oracle(msg) 
-    print(ciphertext)
+    print(f"Detected: { detect(ciphertext) } mode")
 
 if __name__ == "__main__":
     main()
