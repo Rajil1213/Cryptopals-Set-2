cc1a29e5fd4a863b3b59c1f8b74a8766505ab1d7
Rajil1213 <imrb1213@gmail.com>
2020-09-07 13:23:51 +0545

    Completed conversion routine for cookies to objects

---
+++ challenge13.py
@@ -0,0 +1,41 @@
+from collections import OrderedDict
+from Crypto.Cipher import AES
+from Crypto import Random
+import re
+
+class objectify:
+
+    def __init__(self, cookie):
+        self.cookie = cookie
+        self.obj = OrderedDict()
+
+    def convert(self):
+        # get key=value pairs
+        kv = self.cookie.split('&')
+        # assign key=value pairs to dictionary: dict[key]=value
+        for pair in kv:
+            k, v = pair.split('=')
+            self.obj[k] = v
+        return self.obj
+
+    def __repr__(self):
+        self.convert()
+        ret_value = "{\n"
+        last_key = next(reversed(self.obj))
+        for key, value in self.obj.items():
+            if not key == last_key:
+                ret_value += f"\t{ key }: '{ value }',\n"
+            else:
+                ret_value += f"\t{ key }: '{ value }'\n"
+        ret_value += "}"
+        return ret_value
+
+
+def main():
+    cookie = "name=admin&role=admin&id=0"
+    obj = objectify(cookie)
+    print(obj)
+
+
+if __name__ == "__main__":
+    main()
