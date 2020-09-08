3cfab7042d28d7391efaa894ae15f89bc84b31c3
Rajil1213 <imrb1213@gmail.com>
2020-09-08 15:27:09 +0545

    Reorganized functions, removed tests for individual functions, and added comments

--- challenge13.py
+++ challenge13.py
@@ -39,54 +39,52 @@ class objectify:
         return ret_value
 
 
-def get_cookie(user_info):
-    # sanitize the `user_info` to remove '&' and '=' signs
-    global user_cnt
-    user_info = re.sub("&|=", "", user_info)
-    cookie = f"email={ user_info }&uid={ user_cnt }&role=user"
-    user_cnt += 1
-    return cookie
-
-
 def pad(value, size):
     if len(value) % size == 0:
         return value
     padding = size - len(value) % size
     padValue = bytes([padding]) * padding
     return value + padValue
-    
-
-def encrypt_profile(cookie):
-    paddedCookie = bytes(cookie, 'ascii')
-    # Apply PKCS#7 padding if necessary
-    paddedCookie = pad(paddedCookie, AES.block_size)
-    ecb = AES.new(KEY, AES.MODE_ECB)
-    cipherCookie = ecb.encrypt(paddedCookie)
-    return cipherCookie
 
 
 def profile_for(user_info):
-    cookie = get_cookie(user_info)
-    return encrypt_profile(cookie)
+    # get cookie from user_info
+    global user_cnt
+    user_info = re.sub("&|=", "", user_info) # sanitize the `user_info` to remove '&' and '=' signs
+    cookie = f"email={ user_info }&uid={ user_cnt }&role=user"
+    user_cnt += 1
+
+    # encrypt the encoded cookie-info
+    paddedCookie = pad(bytes(cookie, 'ascii'), AES.block_size) # PKCS#7 padding 
+    ecb = AES.new(KEY, AES.MODE_ECB)
+    cipherCookie = ecb.encrypt(paddedCookie)
+
+    return cipherCookie
 
 
 def decrypt_profile(key, cipherCookie):
+
+    # decrypt the `cipherCookie`
     ecb = AES.new(key, AES.MODE_ECB)
     plainCookie = ecb.decrypt(cipherCookie)
+
     # remove PKCS#7 padding
     last_byte = plainCookie[-1]
+    # padding is a numeral whose value ranges from 0 to block_size - 1
     if last_byte in range(AES.block_size - 1):
         padding = bytes([last_byte]) * last_byte
+        # check if the `last_byte` number of bytes are the padding bytes
         if plainCookie[-last_byte:] == padding:
             plainCookie = plainCookie[:-plainCookie[-1]]
     
-    cookie = plainCookie.decode('ascii')
     # parse cookie to object format
+    cookie = plainCookie.decode('ascii')
     obj = objectify(cookie) 
     return cookie, str(obj)
 
 
 def create_admin_profile():
+
     # first create a block so that `email=<x>&uid=<x>&role=` occupies one block_size
     # and value of the role occupies the last block
     cookie_parts = 'email=@gmail.com&uid=2&role='
@@ -115,22 +113,5 @@ def create_admin_profile():
     print(f"Object Created: { obj }")
 
 
-def main():
-    response = "y"
-    """
-    # test individual functions
-    while response.lower() == "y":
-        user_info = input("Enter email id: ")
-        cookie = get_cookie(user_info)
-        print(f"Plain cookie: { cookie }")
-        cipherCookie = encrypt_profile(cookie)
-        print(f"Encrypted cookie: { cipherCookie }")
-        plainCookie, obj = decrypt_profile(KEY, cipherCookie)
-        print(f"Decrypted cookie: { plainCookie }")
-        print(f"Parsed cookie: \n{ obj }")
-        response = input("Add more (y/n)? ")
-    """ 
-    create_admin_profile()
-
 if __name__ == "__main__":
-    main()
+    create_admin_profile()
