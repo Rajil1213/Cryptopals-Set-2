580f0b27665f91b4bdcd9dcbdf07b41442eea91e
Rajil1213 <imrb1213@gmail.com>
2020-09-10 23:03:49 +0545

    Added docstring and comments

--- challenge15.py
+++ challenge15.py
@@ -1,16 +1,29 @@
 def valid_padding(paddedMsg, block_size):
+    """checks if `paddedMsg` has valid PKCS#7 padding for given `block_size`
+
+    Args:
+        paddedMsg (bytes): the padded text
+        block_size (int): the block size that is to be obtained by padding
+
+    Returns:
+        bool: True, if the padding is valid. False, otherwise. 
+    """
+    # if the length of the `paddedMsg` is not a multiple of `block_size`
     if len(paddedMsg) % block_size != 0:
         return False
     
     last_byte = paddedMsg[-1] 
 
+    # if the value of the last_byte is greater than or equal to block_size
     if last_byte >= block_size:
         return False
     
     padValue = bytes([last_byte]) * last_byte
+    # if all the padding bytes are not the same
     if paddedMsg[-last_byte:] != padValue:
         return False
     
+    # if, after removing the padding, the remaining characters are not all printable
     if not paddedMsg[:-last_byte].decode('ascii').isprintable():
         return False
     
@@ -18,7 +31,15 @@ def valid_padding(paddedMsg, block_size):
 
 
 def remove_padding(paddedMsg, block_size):
+    """removes padding from `paddedMsg`, displays error-message if padding is invalid
 
+    Args:
+        paddedMsg (bytes): the message that is padded using PKCS#7 padding
+        block_size (int): the block size that is obtained by said padding
+
+    Raises:
+        ValueError: if the padding is invalid and then, shows an error message
+    """ 
     try:
         if not valid_padding(paddedMsg, block_size):
             raise ValueError
@@ -34,6 +55,8 @@ def remove_padding(paddedMsg, block_size):
 
 
 def test():
+    """tests the `remove_padding()` function for various test cases.
+    """
 
     block_size = 16
 
