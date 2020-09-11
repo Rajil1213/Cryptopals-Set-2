20b0aeb359dac259c23f5b14470079bc0903683f
Rajil1213 <imrb1213@gmail.com>
2020-09-11 20:32:41 +0545

    Added docstring and comments

--- challenge16.py
+++ challenge16.py
@@ -21,6 +21,11 @@ def pad(value, size):
 
 
 class InvalidPaddingError(Exception):
+    """exception class for invalid PKCS#7 padding
+
+    Args:
+        Exception (class): inherits from the in-built `Exception` class
+    """
 
     def __init__(self, paddedMsg, message="has invalid PKCS#7 padding."):
         self.paddedMsg = paddedMsg
@@ -83,13 +88,22 @@ def remove_padding(paddedMsg, block_size):
     unpadded = paddedMsg[:-last_byte]
     return unpadded
 
-
+# this is the dictionary for replacing `=` and `&`
 QUOTE = {b';':b'%3B', b'=':b'%3D'}
 
 KEY = Random.new().read(AES.block_size)
 IV = bytes(AES.block_size) # for simplicity just a bunch of 0's
 
 def cbc_encrypt(input_text):
+    """encrypts a the `input_text` using AES-128 in CBC mode,
+    aftr replacing `:` with `%3B` and `=` with `%3D` in the `input_text`
+
+    Args:
+        input_text (byte): the input to encrypt
+
+    Returns:
+        byte: result of AES-128-CBC(prefix+input_text+suffix)
+    """
 
     prepend = b"comment1=cooking%20MCs;userdata="
     append = b";comment2=%20like%20a%20pound%20of%20bacon"
@@ -107,6 +121,14 @@ def cbc_encrypt(input_text):
 
 
 def check(ciphertext):
+    """checks if the `ciphertext` upon decryption has `;admin=true;` in it
+
+    Args:
+        ciphertext (bytes): the result of an AES-128-CBC encryption
+
+    Returns:
+        bool: True if the plain-text contains `;admin=true;`, False otherwise
+    """
 
     cipher = AES.new(KEY, AES.MODE_CBC, IV)
     plaintext = cipher.decrypt(ciphertext)
@@ -119,17 +141,28 @@ def check(ciphertext):
 
 
 def test():
+    """tests the injection of `;admin=true;` into the cipher-text
+    """
 
+    # send two blocks of just A's
     input_string = b'A' * AES.block_size * 2
     ciphertext = cbc_encrypt(input_string)
-    
+
+    # replace first block of A's with the `required` plain-text   
     required = pad(b";admin=true;", AES.block_size)
+    # xor each byte of the required with each byte of second block i.e, with 'A'
     inject = bytes([r ^ ord('A') for r in required])
 
-    # extra = length of ciphertext - length of injected text - length of prefix = one block of input + suffix
+    # extra = length of ciphertext - length of injected text - length of prefix 
+    # = one block of input + suffix
     extra = len(ciphertext) - len(inject) - 2 * AES.block_size 
+    # keep `inject` fill either side with 0's to match length with original ciphertext
+    # xor with 0 does not change value
+    # this replaces the first block of input with `required` while the rest is unchanged
     inject = bytes(2 * AES.block_size) + inject + bytes(extra)
 
+    # to craft cipher-text, xor the `inject` bytes with
+    # corresponding byte of the ciphertext
     crafted = bytes([x ^ y for x, y in zip(ciphertext, inject)])
 
     if check(crafted):
